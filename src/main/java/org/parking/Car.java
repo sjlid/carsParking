ee3964dfd315bf1fddc78a8a4e01fc034180eb52
Ivan <senjohn.0000@gmail.com>
2024-02-21 02:55:13 +0300

    some refactoring

--- src/main/java/org/parking/Car.java
+++ src/main/java/org/parking/Car.java
@@ -2,30 +2,19 @@ package org.parking;
 
 public class Car {
     /**
-     * так регистрируется в системе госномер, который прилетает с камеры шлагбаума
-     */
-    private String plateNumber;
-    /**
      * время начала парковки в минутах
      */
-    private long parkingStartTime;
+    private final long parkingStartTime;
     /**
      * время конца парковки в минутах
      */
     private long parkingEndTime;
 
-    public Car(String plateNumber, long parkingStartTime) {
-        this.plateNumber = plateNumber;
+    public Car(long parkingStartTime) {
         this.parkingStartTime = parkingStartTime;
-    }
-    public String getPlateNumber() {
-        return plateNumber;
     }
     public long getParkingStartTime() {
         return parkingStartTime;
-    }
-    public void setParkingStartTime(long parkingStartTime) {
-        this.parkingStartTime = parkingStartTime;
     }
     public long getParkingEndTime() {
         return parkingEndTime;
