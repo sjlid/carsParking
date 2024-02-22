1649fe701249e4400c324e49b192f2000726f544
eevan <senjohn.0000@gmail.com>
2024-02-22 23:56:54 +0300

    refactoring

--- src/main/java/org/parking/ControlSystem.java
+++ src/main/java/org/parking/ControlSystem.java
@@ -9,20 +9,11 @@ public class ControlSystem implements IControl {
      * переменная для цикла, чтобы убрать рекурсию
      */
     private boolean userCheck = true;
-    /**
-     * бахаем хэшмап для хранения тачек
-     */
     Map<String, Car> carsOnParking = new HashMap<>();
-    /**
-     * лоукост замена камере шлагбаума
-     */
+
     Scanner scanner = new Scanner(System.in);
 
-    /**
-     * манагер-метод для управления вашей парковочкой
-     */
     public void manageApp() {
-
         while (userCheck) {
             System.out.println("Приезжает или уезжает авто?");
             String userInput = scanner.next();
@@ -50,9 +41,6 @@ public class ControlSystem implements IControl {
         }
     }
 
-    /**
-     * метод для обработки прибытия авто
-     */
     public void carArrive() {
         System.out.println("Какой там госномер-то?");
         String newCar = scanner.next();
@@ -63,9 +51,6 @@ public class ControlSystem implements IControl {
         System.out.println("Отлично! Новое авто может быть на подходе.");
     }
 
-    /**
-     * метод для обработки уезда авто
-     */
     public void carDepart() {
         System.out.println("Какой уезжает госномер?");
         String carNumber = scanner.next();
@@ -74,7 +59,7 @@ public class ControlSystem implements IControl {
             carsOnParking
                     .get(carNumber)
                     .setParkingEndTime(System.currentTimeMillis());
-            checkSum(
+            calculatePayment(
                     carsOnParking.get(carNumber).getParkingStartTime(),
                     carsOnParking.get(carNumber).getParkingEndTime()
             );
@@ -85,33 +70,22 @@ public class ControlSystem implements IControl {
         }
     }
 
-    /**
-     * метод для расчета бабла на оплату
-     */
-    public void checkSum(long timeStart, long timeFinish) {
+    public void calculatePayment(long timeStart, long timeFinish) {
         float totalTime = (float) ((timeFinish - timeStart) / 60000L);
         float payment = totalTime * (getPayment());
         System.out.println("Водитель заплатит " + payment + " рублей");
     }
 
-
-    /**
-     * это если хотим посмотреть, че там по тачкам, которые ЩАС на парковке.
-     * В целом нас интересует только ключ и тачка со своим хэшем
-     */
     public void checkCurrentCars() {
         if (!carsOnParking.isEmpty()) {
             for(Map.Entry<String, Car> entry : carsOnParking.entrySet()) {
-                System.out.println(entry.getKey() + " : " + entry.getValue());
+                System.out.println(entry.getKey() + " сейчас на парковке");
             }
         } else {
             System.out.println("Парковка пуста!");
         }
     }
 
-    /**
-     * цена 1 руб / минута
-     */
     public int getPayment() {
         return 1;
     }
--- src/main/java/org/parking/IControl.java
+++ src/main/java/org/parking/IControl.java
@@ -1,9 +1,28 @@
 package org.parking;
 
 public interface IControl {
+    /**
+     * method for simulating car's arriving
+     */
     void carArrive();
+    /**
+     * method for simulating car's departing
+     */
     void carDepart();
+    /**
+     * method for checking current cars on the parking
+     */
     void checkCurrentCars();
-    void checkSum(long timeStart, long timeFinish);
+    /**
+     * method for calculating payment for parking
+     */
+    void calculatePayment(long timeStart, long timeFinish);
+    /**
+     * method for parking controller
+     */
     void manageApp();
+    /**
+     * method for parking controller
+     */
+    int getPayment();
 }
