e2c09743330d404d5c83c2a9fe0bcf7819361550
Ivan <senjohn.0000@gmail.com>
2024-02-27 14:07:32 +0300

    some refactoring

--- src/main/java/org/parking/ControlSystem.java
+++ src/main/java/org/parking/ControlSystem.java
@@ -1,87 +1,37 @@
 package org.parking;
 
-import java.util.Map;
-import java.util.Scanner;
-
-public class ControlSystem implements ControlInterface {
-    /**
-     * boolean variable for avoiding of recursion
-     */
-    private boolean userCheck = true;
-    final Scanner scanner = new Scanner(System.in);
-    Parking parking = new Parking();
+public class ControlSystem {
+    private final Parking parking = new Parking();
 
     public void manageApp() {
         System.out.println("Приложение поддерживает варианты ввода: приезжает, уезжает, состояние, выход");
-        while (userCheck) {
+        while (true) {
             System.out.println("Приезжает или уезжает авто?");
-            String userInput = scanner.next();
+            String userInput = parking
+                    .getScanner()
+                    .next();
 
             switch (userInput) {
                 case "приезжает":
-                    carArrive();
+                    parking.carArrive();
                     continue;
                 case "уезжает":
-                    if (!parking.carsOnParking.isEmpty()) {
-                        carDepart();
+                    if (!parking
+                            .getCarsOnParking()
+                            .isEmpty()) {
+                        parking.carDepart();
                     } else {
                         System.out.println("У вас нет авто на парковке! Чему там уезжать-то, ты, пес?!");
                     }
                     continue;
                 case "состояние":
-                    checkCarsOnParking();
+                    parking.checkCarsOnParking();
                     continue;
                 case "выход":
-                    userCheck = false;
-                    break;
+                    return;
                 default:
                     System.out.println("Какую-то дичь пишете...");
             }
         }
     }
-
-    public void carArrive() {
-        System.out.println("Какой там госномер-то?");
-        String newCar = scanner.next();
-        parking.carsOnParking.put(
-                newCar,
-                new Car(newCar, System.currentTimeMillis())
-        );
-        System.out.println("Отлично! Новое авто может быть на подходе.");
-    }
-
-    public void carDepart() {
-        System.out.println("Какой уезжает госномер?");
-        String carNumber = scanner.next();
-
-        if (parking.carsOnParking.containsKey(carNumber)) {
-            parking.carsOnParking
-                    .get(carNumber)
-                    .setParkingEndTime(System.currentTimeMillis());
-            float parkingSum = parking.calculatePayment(
-                    parking.carsOnParking
-                            .get(carNumber)
-                            .getParkingStartTime(),
-                    parking.carsOnParking
-                            .get(carNumber)
-                            .getParkingEndTime()
-            );
-            System.out.println("Авто под номером " + carNumber + " уехало.");
-            System.out.println("Водитель оплатит " + parkingSum + " рублей");
-            parking.carsOnParking.remove(carNumber);
-        } else {
-            System.out.println("Что-то напутали с номером машины, такой нет на парковке!");
-        }
-    }
-
-    public void checkCarsOnParking() {
-        if (!parking.carsOnParking.isEmpty()) {
-            for(Map.Entry<String, Car> entry : parking.carsOnParking.entrySet()) {
-                System.out.println(entry.getKey() + " сейчас на парковке");
-            }
-        } else {
-            System.out.println("Парковка пуста!");
-        }
-    }
-
 }
--- src/main/java/org/parking/Parking.java
+++ src/main/java/org/parking/Parking.java
@@ -2,19 +2,63 @@ package org.parking;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Parking {
-    final Map<String, Car> carsOnParking = new HashMap<>();
+    private final Map<String, Car> carsOnParking = new HashMap<>();
+    private final Scanner scanner = new Scanner(System.in);
 
     /**
      * Calculates total sum for car's parking in the end of it
      */
-    public int calculatePayment(long timeStart, long timeFinish) {
-        float totalTime = (float) ((timeFinish - timeStart) / 60000L);
+    public int calculatePayment(Car car) {
+        float totalTime = (float) ((car.getParkingEndTime() - car.getParkingStartTime()) / 60000L);
         return (int) totalTime * (getPayment());
     }
 
     public int getPayment() {
         return 1;
+    }
+    public Map<String, Car> getCarsOnParking() {
+        return carsOnParking;
+    }
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void carArrive() {
+        System.out.println("Какой там госномер-то?");
+        String newCar = scanner.next();
+        carsOnParking.put(
+                newCar,
+                new Car(newCar, System.currentTimeMillis())
+        );
+        System.out.println("Отлично! Новое авто может быть на подходе.");
+    }
+
+    public void carDepart() {
+        System.out.println("Какой уезжает госномер?");
+        String carNumber = scanner.next();
+
+        if (carsOnParking.containsKey(carNumber)) {
+                carsOnParking
+                    .get(carNumber)
+                    .setParkingEndTime(System.currentTimeMillis());
+            float parkingSum = calculatePayment(carsOnParking.get(carNumber));
+            System.out.println("Авто под номером " + carNumber + " уехало.");
+            System.out.println("Водитель оплатит " + parkingSum + " рублей");
+            getCarsOnParking().remove(carNumber);
+        } else {
+            System.out.println("Что-то напутали с номером машины, такой нет на парковке!");
+        }
+    }
+
+    public void checkCarsOnParking() {
+        if (!getCarsOnParking().isEmpty()) {
+            System.out.println("Сейчас на парковке:");
+            System.out.println(carsOnParking.values());
+        } else {
+            System.out.println("Парковка пуста!");
+        }
     }
 }
