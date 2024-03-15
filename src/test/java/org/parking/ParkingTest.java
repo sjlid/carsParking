f3b953dd7fe37d741063433b6540fd65d883df0d
eevan <senjohn.0000@gmail.com>
2024-03-16 00:29:02 +0300

    add tests

--- src/test/java/org/parking/ParkingTest.java
+++ src/test/java/org/parking/ParkingTest.java
@@ -2,12 +2,16 @@ package org.parking;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingTest {
 
     @Test
     void parking_size_cannot_be_overflowed() {
+        //
+    }
+
+    @Test
+    void there_cannot_be_two_cars_with_same_nameplate() {
         //
     }
 
