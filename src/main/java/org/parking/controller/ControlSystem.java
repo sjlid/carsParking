51676fd494326486ffe7fd2b94423abaa481927d
eevan <senjohn.0000@gmail.com>
2024-05-04 22:41:00 +0300

    delete useless class and make few xosmetic stuff

--- src/main/java/org/parking/controller/ControlSystem.java
+++ src/main/java/org/parking/controller/ControlSystem.java
@@ -21,13 +21,11 @@ public class ControlSystem {
         this.parkingDAO = parkingDAO;
     }
 
-
     @GetMapping("/parked_cars")
     public String carsOnParking(Model model) {
         model.addAttribute("cars", parkingDAO.carsOnParking());
         return "parking/parked_cars";
     }
-
 
     @GetMapping("/menu")
     public String showMenu() {
@@ -60,48 +58,4 @@ public class ControlSystem {
         parkingDAO.carDepart(id);
         return "redirect:/parking/menu";
     }
-
-
-
-
-    //старый код
-    /*
-    private final Parking parking;
-    private final Scanner scanner = new Scanner(System.in);
-
-    public ControlSystem(Parking parking) {
-        this.parking = parking;
-    }
-
-    public void manageApp() {
-        System.out.println("The app maintain the following commands: arrive, depart, parking, exit.");
-        while (true) {
-            System.out.println("Is it arriving or departing?");
-            String userInput = scanner.next();
-            switch (userInput) {
-                case "arrive":
-                    System.out.println("What's the nameplate here, bro? It should be not less than 5 characters and no more than 9 characters");
-                    Car car = new Car(new Nameplate(scanner.next()));
-                    parking.carArrive(car);
-                    System.out.println("Cool! A new auto may arrives in the nearest minute.");
-                    continue;
-                case "depart":
-                    System.out.println("What nameplate are leaving us, man?");
-                    String carDeparting = scanner.next();
-                    System.out.println("Payment will be " + parking.carDepart(parking.getCarsOnParking()
-                            .get(carDeparting)) + " rubles");
-                    System.out.println("Car with " + carDeparting + " nameplate has departed.");
-                    continue;
-                case "parking":
-                    System.out.println("Currently we have the following cars here: \n" + parking.checkCarsOnParking());
-                    continue;
-                case "exit":
-                    return;
-                default:
-                    System.out.println("Ah?! I dunno what is it...");
-            }
-        }
-    }
-
-     */
 }
